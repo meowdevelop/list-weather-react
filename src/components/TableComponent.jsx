06b6b24f7d7452f5d4320df051329d0c59c59f79
Nadezhda Nikishina <nadya9449@mail.ru>
2020-02-15 23:49:28 +0400

    Add sort for cities

--- src/components/TableComponent.jsx
+++ src/components/TableComponent.jsx
@@ -6,21 +6,26 @@ import LineComponent from './LineComponent';
 
 class TableComponent extends React.Component {
 
+  reverseCities = (cities) => {
+    cities.reverse();
+    this.props.updateState('ADD_NEW_CITY', cities);
+  }
+
   render() {
 
     const { cities } = this.props.store.weather;
 
     return <div className="table">
         <div className="table__headers">
-            <div className="table__header">City</div>
+            <div className="table__header table__header--city" onClick = {() => this.reverseCities(cities)}>City</div>
             <div className="table__header">Temp</div>
             <div className="table__header">Press</div>
         </div>
         {cities.length === 0 ? (<p className="table__comment">Добавьте город</p>) : null}
         {cities.map((city) =>
-        <LineComponent data={city} />
+        <LineComponent data={city} key={city.city}/>
       )} 
-  </div>
+      </div>
   }
 }
 
